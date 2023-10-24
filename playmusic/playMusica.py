5191e7895182ac6e548426ecdbaa67ce291e5784
lucaspbds <118389176+lucaspbds@users.noreply.github.com>
2023-10-23 22:18:50 -0300

    arquivo para tocar a música e o bd das músicas

---
+++ playmusic/playMusica.py
@@ -0,0 +1,11 @@
+from pygame import mixer, time, USEREVENT
+
+
+def playMusica(playlist):
+    for music in playlist:
+        mixer.music.load(music)
+        mixer.music.play()
+        mixer.music.set_endevent(USEREVENT)
+        while mixer.music.get_busy():
+            time.wait(100)
+    # pygame.mixer.music.play()
